d0cf61921635a62ec05450116a70bfea10ec5fe5
erikalvar <erikalvarbass@gmail.com>
2021-01-12 17:21:52 -0500

    adds material-ui

--- src/SongIndex.js
+++ src/SongIndex.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function SongIndex() {
-  return (
-    <div>
-      <h1>Song Index</h1>
-    </div>
-    
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/songs")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setSongs(result);
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {songs.map(song => (
+          <li key={song.id}>
+            {song.title} {song.composer}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default SongIndex;
